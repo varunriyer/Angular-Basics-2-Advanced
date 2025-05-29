40d0548f967191b376672bdff2f076838f7b368e
varunriyer <vs7835@srmist.edu.in>
2025-05-29 15:39:38 +0530

    feat: update dummy user data with new names and avatars

--- Basics-2-Advanced/src/app/test.users.ts
+++ Basics-2-Advanced/src/app/test.users.ts
@@ -1,32 +1,32 @@
 export const DUMMY_USERS = [
   {
     id: 'u1',
-    name: 'Jasmine Washington',
+    name: 'Aarohi Sharma', // female
     avatar: 'user-1.jpg',
   },
   {
     id: 'u2',
-    name: 'Emily Thompson',
+    name: 'Isha Verma', // female
     avatar: 'user-2.jpg',
   },
   {
     id: 'u3',
-    name: 'Marcus Johnson',
+    name: 'Rohan Mehra', // male
     avatar: 'user-3.jpg',
   },
   {
     id: 'u4',
-    name: 'David Miller',
+    name: 'Vivaan Desai', // male
     avatar: 'user-4.jpg',
   },
   {
     id: 'u5',
-    name: 'Priya Patel',
+    name: 'Saanvi Nair', // female
     avatar: 'user-5.jpg',
   },
   {
     id: 'u6',
-    name: 'Arjun Singh',
+    name: 'Kabir Choudhary', // male
     avatar: 'user-6.jpg',
   },
 ];
