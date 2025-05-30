127c581534cca2358c8d09da1ef10dc4891ea8ce
varunriyer <vs7835@srmist.edu.in>
2025-05-30 11:49:52 +0530

    fix: enforce required inputs for avatar and name in user component

--- Basics-2-Advanced/src/app/user/user.ts
+++ Basics-2-Advanced/src/app/user/user.ts
@@ -6,8 +6,8 @@ import { Component, Input} from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
-  @Input() avatar!:string;
-  @Input() name!: string;
+  @Input({required:true}) avatar!:string;
+  @Input({required:true}) name!: string;
 
   get imagePath(){
     return this.avatar;
