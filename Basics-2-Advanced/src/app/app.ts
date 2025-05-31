87cb2bd71131103a5478644d2c3c0d195852b382
varunriyer <vs7835@srmist.edu.in>
2025-05-31 14:58:11 +0530

    feat: add onSelectUser method to handle user selection in App component

--- Basics-2-Advanced/src/app/app.ts
+++ Basics-2-Advanced/src/app/app.ts
@@ -2,13 +2,18 @@ import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { User } from "./user/user";
 import { DUMMY_USERS } from './test.users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   users = DUMMY_USERS;
+
+  onSelectUser(id:string){
+    console.log(`Selected user with id: ${id}`);
+  }
 }
